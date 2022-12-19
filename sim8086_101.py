321ab19873264b4a46cc6d22e8fbafe11f72a1db
bogumilharnik <117505733+bogumilharnik@users.noreply.github.com>
2022-12-19 18:28:17 +0100

    Update sim8086_101.py

--- sim8086_101.py
+++ sim8086_101.py
@@ -10,14 +10,14 @@ dx = int(input("Podaj liczbę dla rejestru DX: "))
 
 print("wybierz operację: MOV, XCHG lub Q aby wyjść")
 
-wyjscie = "Q"
+wyjscie = str.upper("Q")
 
-while wyjscie == "Q":
+while wyjscie != False:
 
     operacja = str.upper(input("Podaj operację: "))
     operacja1 = "MOV"
     operacja2 = "XCHG"
-    wyjscie = "Q"
+    # wyjscie = "Q"
 
     if operacja == operacja1:
 
@@ -161,148 +161,135 @@ while wyjscie == "Q":
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru AX: " + "ax")
             print(ax)
+            print(bx)
             print("Wykonujemy operację zamiany z rejestru AX do BX")
             ax, bx = bx, ax
             print("Odczytujemy zawartość rejestru BX: " + "bx")
             print(bx)
+            print(ax)
 
         elif nr_rejestru_z == 'AX' and nr_rejestru_do == 'CX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru AX: " + "ax")
             print(ax)
+            print(cx)
             print("Wykonujemy operację zamiany z rejestru AX do CX")
             ax, cx = cx, ax
-            print("Odczytujemy zawartość rejestru BX: " + "bx")
+            print("Odczytujemy zawartość rejestru CX: " + "cx")
             print(cx)
+            print(ax)
 
         elif nr_rejestru_z == 'AX' and nr_rejestru_do == 'DX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru AX: " + "ax")
             print(ax)
-            print("Wykonujemy operację zamiany z rejestru AX do DX")
-            ax, dx = dx, ax
-            print("Odczytujemy zawartość rejestru BX: " + "dx")
-            print(cx)
-
-        elif nr_rejestru_z == 'BX' and nr_rejestru_do == 'CX':
-            print("Wybrana operacja zamiany: " + operacja1)
-            print("Pobieramy zawartość rejestru AX: " + "ax")
-            print(ax)
-            print("Wykonujemy operację zamiany z rejestru BX do CX")
-            bx, cx = cx, bx
-            print("Odczytujemy zawartość rejestru BX: " + "bx")
-            print(cx)
-
-        elif nr_rejestru_z == 'AX' and nr_rejestru_do == 'BX':
-            print("Wybrana operacja zamiany: " + operacja1)
-            print("Pobieramy zawartość rejestru AX: " + "ax")
-            print(ax)
-            print("Wykonujemy operację zamiany z rejestru AX do BX")
-            ax, bx = bx, ax
-            print("Odczytujemy zawartość rejestru BX: " + "bx")
-            print(bx)
-
-        elif nr_rejestru_z == 'AX' and nr_rejestru_do == 'CX':
-            print("Wybrana operacja zamiany: " + operacja1)
-            print("Pobieramy zawartość rejestru AX: " + "ax")
-            print(ax)
-            print("Wykonujemy operację zamiany z rejestru AX do CX")
-            ax, cx = cx, ax
-            print("Odczytujemy zawartość rejestru CX: " + "bx")
-            print(cx)
-
-        elif nr_rejestru_z == 'AX' and nr_rejestru_do == 'DX':
-            print("Wybrana operacja zamiany: " + operacja1)
-            print("Pobieramy zawartość rejestru AX: " + "ax")
-            print(ax)
+            print(dx)
             print("Wykonujemy operację zamiany z rejestru AX do DX")
             ax, dx = dx, ax
             print("Odczytujemy zawartość rejestru DX: " + "dx")
             print(dx)
+            print(ax)
 
         elif nr_rejestru_z == 'BX' and nr_rejestru_do == 'AX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru BX: " + "bx")
             print(bx)
+            print(ax)
             print("Wykonujemy operację zamiany z rejestru BX do AX")
             bx, ax = ax, bx
             print("Odczytujemy zawartość rejestru AX: " + "ax")
             print(ax)
+            print(bx)
 
         elif nr_rejestru_z == 'BX' and nr_rejestru_do == 'CX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru BX: " + "bx")
             print(bx)
+            print(cx)
             print("Wykonujemy operację zamiany z rejestru BX do CX")
             bx, cx = cx, bx
-            print("Odczytujemy zawartość rejestru CX: " + "cx")
+            print("Odczytujemy zawartość rejestru BX: " + "bx")
             print(cx)
+            print(bx)
 
         elif nr_rejestru_z == 'BX' and nr_rejestru_do == 'DX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru BX: " + "bx")
             print(bx)
-            print("Wykonujemy operację zamiany z rejestru BX do DX")
-            bx, dx = dx, bx
-            print("Odczytujemy zawartość rejestru DX: " + "ax")
             print(dx)
+            print("Wykonujemy operację zamiany z rejestru AX do CX")
+            bx, dx = dx, bx
+            print("Odczytujemy zawartość rejestru DX: " + "dx")
+            print(dx)
+            print(bx)
 
         elif nr_rejestru_z == 'CX' and nr_rejestru_do == 'AX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru CX: " + "cx")
             print(cx)
+            print(ax)
             print("Wykonujemy operację zamiany z rejestru CX do AX")
-            cx, ax = cx, bx
+            cx, ax = ax, cx
             print("Odczytujemy zawartość rejestru AX: " + "ax")
+            print(cx)
             print(ax)
 
         elif nr_rejestru_z == 'CX' and nr_rejestru_do == 'BX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru CX: " + "cx")
             print(cx)
+            print(bx)
             print("Wykonujemy operację zamiany z rejestru CX do BX")
             cx, bx = bx, cx
             print("Odczytujemy zawartość rejestru BX: " + "bx")
             print(bx)
+            print(cx)
 
         elif nr_rejestru_z == 'CX' and nr_rejestru_do == 'DX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru CX: " + "cx")
             print(cx)
+            print(dx)
             print("Wykonujemy operację zamiany z rejestru CX do DX")
             cx, dx = dx, cx
             print("Odczytujemy zawartość rejestru DX: " + "dx")
             print(dx)
+            print(cx)
 
         elif nr_rejestru_z == 'DX' and nr_rejestru_do == 'AX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru DX: " + "dx")
-            print(dx,ax)
+            print(dx)
+            print(ax)
             print("Wykonujemy operację zamiany z rejestru DX do AX")
             dx, ax = ax, dx
-            print("Odczytujemy zawartość rejestru AX: " + "ax")
-            print(ax,dx)
+            print("Odczytujemy zawartość rejestru DX: " + "ax")
+            print(ax)
+            print(dx)
 
         elif nr_rejestru_z == 'DX' and nr_rejestru_do == 'BX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru DX: " + "dx")
             print(dx)
+            print(bx)
             print("Wykonujemy operację zamiany z rejestru DX do AX")
             dx, bx = bx, dx
             print("Odczytujemy zawartość rejestru BX: " + "bx")
-            print(ax)
+            print(bx)
+            print(dx)
 
         elif nr_rejestru_z == 'DX' and nr_rejestru_do == 'CX':
             print("Wybrana operacja zamiany: " + operacja1)
             print("Pobieramy zawartość rejestru DX: " + "dx")
             print(dx)
-            print("Wykonujemy operację zamiany z rejestru DX do AX")
+            print(cx)
+            print("Wykonujemy operację zamiany z rejestru DX do CX")
             dx, cx = cx, dx
             print("Odczytujemy zawartość rejestru CX: " + "cx")
             print(cx)
+            print(dx)
 
     if operacja == "Q":
         break
 
 print("Zakończenie symulatora")
-
